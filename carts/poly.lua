ddc0c5b759353192c0c77599ae70c778bc269894
freds <frederic.souchu@laposte.net>
2021-03-09 22:33:32 +0100

    minor texture mapping optimization

--- carts/poly.lua
+++ carts/poly.lua
@@ -33,19 +33,15 @@ function polyline(v,c)
 end
 
 function tpoly(v)
-	local p0,spans=v[#v],{}
-	local x0,y0,w0=p0.x,p0.y,p0.w
-	local u0,v0=p0.u*w0,p0.v*w0
+	local nv,spans=#v,{}
 	-- ipairs is slower for small arrays
 	for i=1,#v do
-		local p1=v[i]
-		local x1,y1,w1=p1.x,p1.y,p1.w
-		local u1,v1=p1.u*w1,p1.v*w1
-		local _x1,_y1,_w1,_u1,_v1=x1,y1,w1,u1,v1
+		local p0,p1=v[i%nv+1],v[i]
+		local x0,y0,w0,x1,y1,w1=p0.x,p0.y,p0.w,p1.x,p1.y,p1.w
+		local u0,v0,u1,v1=p0.u*w0,p0.v*w0,p1.u*w1,p1.v*w1
 		if(y0>y1) x0,y0,x1,y1,w0,w1,u0,v0,u1,v1=x1,y1,x0,y0,w1,w0,u1,v1,u0,v0
 		local dy=y1-y0
-		local dx,dw,du,dv=(x1-x0)/dy,(w1-w0)/dy,(u1-u0)/dy,(v1-v0)/dy
-		local cy0=y0\1+1
+		local cy0,dx,dw,du,dv=y0\1+1,(x1-x0)/dy,(w1-w0)/dy,(u1-u0)/dy,(v1-v0)/dy
 		if(y0<0) x0-=y0*dx w0-=y0*dw u0-=y0*du v0-=y0*dv y0=0 cy0=0
 		-- sub-pix shift
 		local sy=cy0-y0
@@ -58,8 +54,8 @@ function tpoly(v)
 			local span=spans[y]
 			if span then
 				-- backup current edge values
-				local b,bu,bv,bw,a,au,av,aw=x0,u0,v0,w0,unpack(span)
-				if(a>b) a,au,av,aw,b,bu,bv,bw=b,bu,bv,bw,unpack(span)
+				local b,bu,bv,bw,a,au,av,aw=x0,u0,v0,w0,span.x,span.u,span.v,span.w
+				if(a>b) a,au,av,aw,b,bu,bv,bw=b,bu,bv,bw,span.x,span.u,span.v,span.w
 			 
 				local x0,x1=a\1+1,b\1
 				if(x1>127) x1=127
@@ -74,45 +70,33 @@ function tpoly(v)
 					aw+=sa*daw
 					
 					-- 4-pixel stride deltas
-					dau<<=2
-					dav<<=2
-					daw<<=2
-
+					dau<<=3
+					dav<<=3
+					daw<<=3
+					
 						-- faster but produces edge artifacts
 						-- local du,dv=(bu/bw-au/aw)/dab,(bv/bw-av/aw)/dab
 
 					-- clip right span edge
 					poke(0x5f22,x1+1)
 
-					for x=x0,x1,4 do
+					for x=x0,x1,8 do
 						local u,v=au/aw,av/aw
 						aw+=daw
-						tline(x,y,x+3,y,u,v,((dau-u*daw)>>2)/aw,((dav-v*daw)>>2)/aw)
+						tline(x,y,x+7,y,u,v,((dau-u*daw)>>3)/aw,((dav-v*daw)>>3)/aw)
 						au+=dau
 						av+=dav
 					end
 				end
 			else
-				spans[y]={x0,u0,v0,w0}
+				spans[y]={x=x0,u=u0,v=v0,w=w0}
 			end
 			x0+=dx
 			w0+=dw
 			u0+=du
 			v0+=dv
 		end
-		x0,y0,w0,u0,v0=_x1,_y1,_w1,_u1,_v1
 	end
-	--[[
-	local v0,v1,v2,v3=
-		v[1],
-		v[2],
-		v[3],
-		v[4]
-	line(v0.x,v0.y,v1.x,v1.y,7)
-	line(v1.x,v1.y,v2.x,v2.y,7)
-	line(v2.x,v2.y,v3.x,v3.y,7)
-	line(v3.x,v3.y,v0.x,v0.y,7)
-	]]
 end
 
 function mempoly(v,col)
